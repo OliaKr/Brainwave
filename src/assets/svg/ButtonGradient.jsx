aa251ac73d1628cc94e7946805d8fe53581377d0
OliaK <olgamr@walla.com>
2024-07-11 23:52:37 +0300

    Adding Header component and reusable Button component

--- src/assets/svg/ButtonGradient.jsx
+++ src/assets/svg/ButtonGradient.jsx
@@ -1,18 +1,58 @@
 const ButtonGradient = () => {
   return (
-    <svg className="block" width={0} height={0}>
+    <svg
+      className="block"
+      width={0}
+      height={0}
+    >
       <defs>
-        <linearGradient id="btn-left" x1="50%" x2="50%" y1="0%" y2="100%">
-          <stop offset="0%" stopColor="#89F9E8" />
-          <stop offset="100%" stopColor="#FACB7B" />
+        <linearGradient
+          id="btn-left"
+          x1="50%"
+          x2="50%"
+          y1="0%"
+          y2="100%"
+        >
+          <stop
+            offset="0%"
+            stopColor="#89F9E8"
+          />
+          <stop
+            offset="100%"
+            stopColor="#FACB7B"
+          />
         </linearGradient>
-        <linearGradient id="btn-top" x1="100%" x2="0%" y1="50%" y2="50%">
-          <stop offset="0%" stopColor="#D87CEE" />
-          <stop offset="100%" stopColor="#FACB7B" />
+        <linearGradient
+          id="btn-top"
+          x1="100%"
+          x2="0%"
+          y1="50%"
+          y2="50%"
+        >
+          <stop
+            offset="0%"
+            stopColor="#D87CEE"
+          />
+          <stop
+            offset="100%"
+            stopColor="#FACB7B"
+          />
         </linearGradient>
-        <linearGradient id="btn-bottom" x1="100%" x2="0%" y1="50%" y2="50%">
-          <stop offset="0%" stopColor="#9099FC" />
-          <stop offset="100%" stopColor="#89F9E8" />
+        <linearGradient
+          id="btn-bottom"
+          x1="100%"
+          x2="0%"
+          y1="50%"
+          y2="50%"
+        >
+          <stop
+            offset="0%"
+            stopColor="#9099FC"
+          />
+          <stop
+            offset="100%"
+            stopColor="#89F9E8"
+          />
         </linearGradient>
         <linearGradient
           id="btn-right"
@@ -21,8 +61,14 @@ const ButtonGradient = () => {
           y1="0%"
           y2="100%"
         >
-          <stop offset="0%" stopColor="#9099FC" />
-          <stop offset="100%" stopColor="#D87CEE" />
+          <stop
+            offset="0%"
+            stopColor="#9099FC"
+          />
+          <stop
+            offset="100%"
+            stopColor="#D87CEE"
+          />
         </linearGradient>
       </defs>
     </svg>
