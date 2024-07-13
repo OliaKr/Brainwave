dd372d2af8d2b6c195d662028d1a6864b1341d58
OliaK <olgamr@walla.com>
2024-07-13 20:36:35 +0300

    Adding and styling the Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./cmps/Header";
+import Hero from "./cmps/Hero";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
