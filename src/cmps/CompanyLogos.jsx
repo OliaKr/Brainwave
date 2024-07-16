b1ea93e6b54827d5efd6e0feef7e2b4893badb2f
OliaK <olgamr@walla.com>
2024-07-17 00:10:55 +0300

    Adding notification and company logos to Hero component

---
+++ src/cmps/CompanyLogos.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { companyLogos } from "../constants";
+
+const CompanyLogos = ({ className }) => {
+  return (
+    <div className={className}>
+      <h5 className="tagline mb-6 text-center text-n-1/50">
+        Helping people create beautiful content at
+      </h5>
+      <ul className="flex">
+        {companyLogos.map((logo, title) => (
+          <li
+            className="flex items-center justify-center flex-1 h-[8.5rem]"
+            key={title}
+          >
+            <img
+              src={logo}
+              width={134}
+              height={28}
+              alt={logo}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CompanyLogos;
--- src/cmps/Hero.jsx
+++ src/cmps/Hero.jsx
@@ -6,12 +6,15 @@ import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
 import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
+import Generating from "./Generating";
+import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
   const parallaxRef = useRef(null);
   return (
     <Section
-      className="pt-[12rem] -mt-[5.25rem]"
+      className="pt-[12rem] -mt-[5.25rem] "
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
       customPaddings
@@ -54,7 +57,7 @@ const Hero = () => {
             <div className="relative bg-n-8 rounded-[1rem]">
               <div className="h-[1.4rem] bg-n-10 rounded-t-[0.8rem]" />
               <div
-                className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden
+                className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden 
                 md:aspect-[688/490] lg:aspect-[1024/490]"
               >
                 <img
@@ -65,6 +68,12 @@ const Hero = () => {
                   height={490}
                   alt="AI"
                 />
+
+                <Generating
+                  className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto
+                md:bottom-8 md:w-[31rem] md:-translate-x-1/2"
+                />
+
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul
                     className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1
@@ -86,6 +95,13 @@ const Hero = () => {
                     ))}
                   </ul>
                 </ScrollParallax>
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notification
+                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem]
+                  xl:flex"
+                    title="Code generation"
+                  />
+                </ScrollParallax>
               </div>
             </div>
 
@@ -105,6 +121,7 @@ const Hero = () => {
           </div>
           <BackgroundCircles />
         </div>
+        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
       </div>
       <BottomLine />
     </Section>
