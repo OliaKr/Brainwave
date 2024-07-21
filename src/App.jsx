1e999a4b1fd73a95a278cdaefaf257a2a23c9d8e
OliaK <olgamr@walla.com>
2024-07-21 19:27:26 +0300

    Finished styling Roadmap component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Collaboration from "./cmps/Collaboration";
 import Header from "./cmps/Header";
 import Hero from "./cmps/Hero";
 import Pricing from "./cmps/Pricing";
+import Roadmap from "./cmps/Roadmap";
 import Services from "./cmps/Services";
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
       </div>
       <ButtonGradient />
     </>
