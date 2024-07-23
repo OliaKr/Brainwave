17b4f97db2a0f711a032c0bd7bfa017e2edcc294
OliaK <olgamr@walla.com>
2024-07-23 19:20:27 +0300

    Updated paths for benefits SVGs and moved to public directory

--- src/assets/index.js
+++ src/assets/index.js
@@ -58,17 +58,17 @@ import notification2 from "./notification/image-2.png";
 import notification3 from "./notification/image-3.png";
 import notification4 from "./notification/image-4.png";
 
-import benefitCard1 from "./benefits/card-1.svg";
-import benefitCard2 from "./benefits/card-2.svg";
-import benefitCard3 from "./benefits/card-3.svg";
-import benefitCard4 from "./benefits/card-4.svg";
-import benefitCard5 from "./benefits/card-5.svg";
-import benefitCard6 from "./benefits/card-6.svg";
-import benefitIcon1 from "./benefits/icon-1.svg";
-import benefitIcon2 from "./benefits/icon-2.svg";
-import benefitIcon3 from "./benefits/icon-3.svg";
-import benefitIcon4 from "./benefits/icon-4.svg";
-import benefitImage2 from "./benefits/image-2.png";
+// import benefitCard1 from "./benefits/card-1.svg";
+// import benefitCard2 from "./benefits/card-2.svg";
+// import benefitCard3 from "./benefits/card-3.svg";
+// import benefitCard4 from "./benefits/card-4.svg";
+// import benefitCard5 from "./benefits/card-5.svg";
+// import benefitCard6 from "./benefits/card-6.svg";
+// import benefitIcon1 from "./benefits/icon-1.svg";
+// import benefitIcon2 from "./benefits/icon-2.svg";
+// import benefitIcon3 from "./benefits/icon-3.svg";
+// import benefitIcon4 from "./benefits/icon-4.svg";
+// import benefitImage2 from "./benefits/image-2.png";
 
 import discordBlack from "./socials/discord.svg";
 import facebook from "./socials/facebook.svg";
@@ -130,17 +130,17 @@ export {
   notification2,
   notification3,
   notification4,
-  benefitCard1,
-  benefitCard2,
-  benefitCard3,
-  benefitCard4,
-  benefitCard5,
-  benefitCard6,
-  benefitIcon1,
-  benefitIcon2,
-  benefitIcon3,
-  benefitIcon4,
-  benefitImage2,
+  // benefitCard1,
+  // benefitCard2,
+  // benefitCard3,
+  // benefitCard4,
+  // benefitCard5,
+  // benefitCard6,
+  // benefitIcon1,
+  // benefitIcon2,
+  // benefitIcon3,
+  // benefitIcon4,
+  // benefitImage2,
   discordBlack,
   facebook,
   instagram,
